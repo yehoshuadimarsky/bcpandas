398b8175e7ed1a1a64aef4d215c10362de17d3a3
Josh Dimarsky <24758845+yehoshuadimarsky@users.noreply.github.com>
2019-08-05 22:03:45 -0400

    Update __init__.py

--- bcpandas/__init__.py
+++ bcpandas/__init__.py
@@ -11,8 +11,9 @@ __version__ = "0.1.0"
 # BCP check
 cmds = [['bcp','-v'], ['sqlcmd','-?']]
 for cmd in cmds:
-    res = subprocess.run(cmd, stdout=subprocess.PIPE)
-    if res.returncode:
+    try:
+        res = subprocess.run(cmd)
+    except FileNotFoundError:
         warnings.warn(f"{cmd[0].upper()} utility not installed or not found in PATH, bcpandas will not work!")
 
 del subprocess, warnings, cmd, res
