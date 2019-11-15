f47c92ddfd36e06b9af9b2d8cd192dcca7c773d7
Josh Dimarsky <24758845+yehoshuadimarsky@users.noreply.github.com>
2019-11-14 23:52:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: SQL",
     ],
 )
