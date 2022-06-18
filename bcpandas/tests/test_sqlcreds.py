99d08fe9ddb28b23abfb1f1a92602016486139ee
Daniel Eschman <deschman@indians.com>
2022-06-18 18:56:22 -0400

    fix version not specified test

--- bcpandas/tests/test_sqlcreds.py
+++ bcpandas/tests/test_sqlcreds.py
@@ -86,7 +86,7 @@ def test_sql_creds_for_username_password_version_not_specified():
 
     url_split = str(creds.engine.url).split(';')
     url_driver = url_split[0]
-    url_driver_no_version = ''.join([l for l in url_driver.split('') if not l.isnumeric()])
+    url_driver_no_version = ''.join([l for l in url_driver if not l.isnumeric()])
     url_other = url_split[1:].join(';')
 
     assert creds.server == "test_server"
