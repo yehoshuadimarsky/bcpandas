b30a817998fc416a07f06bdd10c9abefa02e92c2
jacobshaw42 <jacob.shaw42@gmail.com>
2023-07-20 00:21:31 -0500

    removing merge conflict

--- bcpandas/main.py
+++ bcpandas/main.py
@@ -114,11 +114,7 @@ class SqlCreds:
             db_url += ";".join(f"{k}={v}" for k, v in odbc_kwargs.items())
         conn_string = f"mssql+pyodbc:///?odbc_connect={quote_plus(db_url)}"
         self.engine = sa.engine.create_engine(conn_string)
-<<<<<<< HEAD
         engine_msg = sub('PWD%3D.*%3B', 'PWD%3D[REDACTED]%3B', str(self.engine))
-=======
-        engine_msg = sub(r"PWD\%3D.*\%3B", "PWD%3D[REDACTED]\%3B", str(self.engine))
->>>>>>> a666b1c6e24aec978158a206e35768fb2ededab9
 
         logger.info(f"Created engine for sqlalchemy:\t{engine_msg}")
 
