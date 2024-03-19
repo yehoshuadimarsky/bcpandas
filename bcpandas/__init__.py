c9d46a58d1b4963195533fe712048395a26efebf
aaronepinto <aaronepinto@icloud.com>
2024-03-19 00:13:20 -0400

    Add error handling for BCP utility check

--- bcpandas/__init__.py
+++ bcpandas/__init__.py
@@ -18,6 +18,10 @@ try:
     run(["bcp", "-v"], stdout=DEVNULL, stderr=DEVNULL, stdin=DEVNULL)
 except FileNotFoundError:
     warnings.warn("BCP utility not installed or not found in PATH, bcpandas will not work!")
+except PermissionError:
+    warnings.warn("BCP utility not found due to permission issues, consider supplying path to bcp or bcpandas will not work!")
+except Exception as e:
+    warnings.warn(f"An unhandled error occurred while checking for BCP utility: {e}")
 
 del run, DEVNULL, warnings
 
